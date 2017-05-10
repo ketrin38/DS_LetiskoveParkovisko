8a167556a2bbf11ebd28a66ffdbb34e4ebea0957
tonnas <illotomas0@gmail.com>
2017-05-10 19:19:52 +0200

    zmena insertov

--- 3.semestralka/data/DATA_HISTORIA_PARKOVANIA/insert_historia_parkovania.sql
+++ 3.semestralka/data/DATA_HISTORIA_PARKOVANIA/insert_historia_parkovania.sql
@@ -3,23 +3,23 @@ INSERT INTO historia_parkovania(id_historia, id_miesta, spz, odkedy)
 VALUES (500,640, 'BA486UC',sysdate);
 
 INSERT INTO historia_parkovania(id_historia, id_miesta, spz, odkedy) 
-VALUES (501,640, 'BA486UC','10.02.2016');
+VALUES (501,640, 'BA486UC',to_date('10.02.2016', 'DD.MM.YYYY'));
 
 INSERT INTO historia_parkovania(id_historia, id_miesta, spz, odkedy) 
-VALUES (502,640, 'BA486UC','08.04.2017');
+VALUES (502,640, 'BA486UC',to_date('08.04.2017', 'DD.MM.YYYY'));
 
 -- NAKLADNE AUTA - STALI ZAMESTNANCI A FIRMY 
 INSERT INTO historia_parkovania(id_historia, id_miesta, spz, odkedy) 
-VALUES (503,640, 'BA030BK','07.05.2017');
+VALUES (503,640, 'BA030BK',to_date('07.05.2017', 'DD.MM.YYYY'));
 
 INSERT INTO historia_parkovania(id_historia, id_miesta, spz, odkedy) 
-VALUES (504,640, 'BA456AB','08.05.2017');
+VALUES (504,640, 'BA456AB',to_date('08.05.2017', 'DD.MM.YYYY'));
 
 INSERT INTO historia_parkovania(id_historia, id_miesta, spz, odkedy) 
-VALUES (505,640, 'BA456AB','09.05.2017');
+VALUES (505,640, 'BA456AB',to_date('09.05.2017', 'DD.MM.YYYY'));
 
 INSERT INTO historia_parkovania(id_historia, id_miesta, spz, odkedy) 
-VALUES (506,640, 'BA456AC','10.05.2017');
+VALUES (506,640, 'BA456AC',to_date('10.05.2017', 'DD.MM.YYYY'));
 
 INSERT INTO historia_parkovania(id_historia, id_miesta, spz, odkedy) 
 VALUES (507,640, 'BA456AC',to_date('11.05.2017 14:25:12', 'DD.MM.YYYY HH24:MI:SS'));
@@ -31,7 +31,7 @@ INSERT INTO historia_parkovania(id_historia, id_miesta, spz, odkedy, dokedy)
 VALUES (509,640, 'BA456AC',to_date('12.05.2017 15:25:12', 'DD.MM.YYYY HH24:MI:SS'), to_date('13.05.2017 14:25:12', 'DD.MM.YYYY HH24:MI:SS'));
 
 INSERT INTO historia_parkovania(id_historia, id_miesta, spz, odkedy) 
-VALUES (510,640, 'BA456AC','14.05.2017');
+VALUES (510,640, 'BA456AC',to_date('14.05.2017', 'DD.MM.YYYY'));
 
 INSERT INTO historia_parkovania(id_historia, id_miesta, spz, odkedy, dokedy) 
 VALUES (511,640, 'BA030BK',to_date('15.05.2017 07:25:12', 'DD.MM.YYYY HH24:MI:SS'), to_date('15.05.2017 09:48:34', 'DD.MM.YYYY HH24:MI:SS'));
@@ -49,22 +49,22 @@ INSERT INTO historia_parkovania(id_historia, id_miesta, spz, odkedy, dokedy)
 VALUES (514,640, 'BA762XF',to_date('16.04.2017 14:48:32', 'DD.MM.YYYY HH24:MI:SS'), to_date('16.04.2017 13:48:34', 'DD.MM.YYYY HH24:MI:SS'));
 
 INSERT INTO historia_parkovania(id_historia, id_miesta, spz, odkedy) 
-VALUES (515,640, 'BA456AC','19.03.2017');
+VALUES (515,640, 'BA456AC',to_date('19.03.2017', 'DD.MM.YYYY'));
 
 ----------------------------------
 
 
 INSERT INTO historia_parkovania(id_historia, id_miesta, spz, odkedy) 
-VALUES (516,640, 'BA030BK','07.03.2017');
+VALUES (516,640, 'BA030BK',to_date('07.03.2017', 'DD.MM.YYYY'));
 
 INSERT INTO historia_parkovania(id_historia, id_miesta, spz, odkedy) 
-VALUES (517,640, 'BA456AB','08.03.2017');
+VALUES (517,640, 'BA456AB',to_date('08.03.2017', 'DD.MM.YYYY'));
 
 INSERT INTO historia_parkovania(id_historia, id_miesta, spz, odkedy) 
-VALUES (518,640, 'BA456AB','09.03.2017');
+VALUES (518,640, 'BA456AB',to_date('09.03.2017', 'DD.MM.YYYY'));
 
 INSERT INTO historia_parkovania(id_historia, id_miesta, spz, odkedy) 
-VALUES (519,640, 'BA456AC','10.03.2017');
+VALUES (519,640, 'BA456AC',to_date('10.03.2017', 'DD.MM.YYYY'));
 
 INSERT INTO historia_parkovania(id_historia, id_miesta, spz, odkedy) 
 VALUES (520,640, 'BA456AC',to_date('11.03.2017 14:25:12', 'DD.MM.YYYY HH24:MI:SS'));
@@ -76,7 +76,7 @@ INSERT INTO historia_parkovania(id_historia, id_miesta, spz, odkedy, dokedy)
 VALUES (522,640, 'BA456AC',to_date('12.03.2017 15:25:12', 'DD.MM.YYYY HH24:MI:SS'), to_date('13.03.2017 14:25:12', 'DD.MM.YYYY HH24:MI:SS'));
 
 INSERT INTO historia_parkovania(id_historia, id_miesta, spz, odkedy) 
-VALUES (523,640, 'BA456AC','14.03.2017');
+VALUES (523,640, 'BA456AC',to_date('14.03.2017', 'DD.MM.YYYY'));
 
 INSERT INTO historia_parkovania(id_historia, id_miesta, spz, odkedy, dokedy) 
 VALUES (524,640, 'BA030BK',to_date('15.03.2017 07:25:12', 'DD.MM.YYYY HH24:MI:SS'), to_date('15.03.2017 09:48:34', 'DD.MM.YYYY HH24:MI:SS'));
@@ -96,27 +96,19 @@ VALUES (526,640, 'BA189BI',to_date('15.03.2017 14:48:32', 'DD.MM.YYYY HH24:MI:SS
 -- BA448DA
 -- BA494EA
 -- BA831HS
-SELECT * 
-FROM vozidlo
-JOIN preukaz
-USING (id_preukaz)
-JOIN zakaznik
-USING (id_zakaznik)
-WHERE typ_vozidla = 'O'
-AND typ = 'S';
 
 
 INSERT INTO historia_parkovania(id_historia, id_miesta, spz, odkedy) 
-VALUES (527,1, 'BA448DA','07.03.2017');
+VALUES (527,1, 'BA448DA',to_date('07.03.2017', 'DD.MM.YYYY'));
 
 INSERT INTO historia_parkovania(id_historia, id_miesta, spz, odkedy) 
-VALUES (528,1, 'BA448DA','08.03.2017');
+VALUES (528,1, 'BA448DA',to_date('08.03.2017', 'DD.MM.YYYY'));
 
 INSERT INTO historia_parkovania(id_historia, id_miesta, spz, odkedy) 
-VALUES (529,1, 'BA448DA','09.03.2017');
+VALUES (529,1, 'BA448DA',to_date('09.03.2017', 'DD.MM.YYYY'));
 
 INSERT INTO historia_parkovania(id_historia, id_miesta, spz, odkedy) 
-VALUES (530,1, 'BA448DA','10.03.2017');
+VALUES (530,1, 'BA448DA',to_date('10.03.2017', 'DD.MM.YYYY'));
 
 INSERT INTO historia_parkovania(id_historia, id_miesta, spz, odkedy) 
 VALUES (531,1, 'BA448DA',to_date('11.03.2017 14:25:12', 'DD.MM.YYYY HH24:MI:SS'));
@@ -128,7 +120,7 @@ INSERT INTO historia_parkovania(id_historia, id_miesta, spz, odkedy, dokedy)
 VALUES (533,1, 'BA345LO',to_date('12.03.2017 15:25:12', 'DD.MM.YYYY HH24:MI:SS'), to_date('13.03.2017 14:25:12', 'DD.MM.YYYY HH24:MI:SS'));
 
 INSERT INTO historia_parkovania(id_historia, id_miesta, spz, odkedy) 
-VALUES (534,1, 'BA345LO','14.03.2017');
+VALUES (534,1, 'BA345LO',to_date('14.03.2017', 'DD.MM.YYYY'));
 
 INSERT INTO historia_parkovania(id_historia, id_miesta, spz, odkedy, dokedy) 
 VALUES (535,1, 'BA011OT',to_date('15.03.2017 07:25:12', 'DD.MM.YYYY HH24:MI:SS'), to_date('15.03.2017 09:48:34', 'DD.MM.YYYY HH24:MI:SS'));
@@ -158,25 +150,3 @@ VALUES (542,1, 'BA011OT',to_date('02.09.2016 16:48:32', 'DD.MM.YYYY HH24:MI:SS')
 
 INSERT INTO historia_parkovania(id_historia, id_miesta, spz, odkedy, dokedy) 
 VALUES (543,1, 'BA448DA',to_date('02.09.2016 17:48:32', 'DD.MM.YYYY HH24:MI:SS'), to_date('03.09.2016 15:48:34', 'DD.MM.YYYY HH24:MI:SS'));
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-SELECT *
-FROM vozidlo
-JOIN preukaz
-USING (id_preukaz)
-WHERE typ_vozidla = 'N';
